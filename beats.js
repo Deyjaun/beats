59c9e4536078cf9b811e441b7aa74498835973c8
Deyjaun Lawrence <Dlawrence@brooklynsteamcenter.org>
2023-05-04 14:51:29 -0400

    added sound leangth

---
+++ beats.js
@@ -0,0 +1,14 @@
+let myForm = document.getElementById("myForm")
+
+let synth = new Tone.Synth().toDestination()
+
+function onSubmit (e) {
+    e.preventDefault()
+
+    synth.triggerAttack(myForm.elements["yourName"].value)
+    let duration = parseFloat (myForm.elements["duration"].value)
+
+synth.triggerRelease(Tone.now() + duration)
+}
+
+myForm.addEventListener("submit" , onSubmit)
