b4dac04e33a37cafc1023d2f7953f1ceab91af96
Deyjaun Lawrence <Dlawrence@brooklynsteamcenter.org>
2023-05-05 14:53:03 -0400

    added if statement

--- beats.js
+++ beats.js
@@ -5,7 +5,7 @@ let synth = new Tone.Synth().toDestination()
 function onSubmit (e) {
     e.preventDefault()
 
-    synth.triggerAttack(myForm.elements["yourName"].value)
+    synth.triggerAttack(myForm.elem ents["yourName"].value)
     let duration = parseFloat (myForm.elements["duration"].value)
 
 synth.triggerRelease(Tone.now() + duration)
